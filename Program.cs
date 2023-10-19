0b0fb0d5cd2dd2e01de38a193992f3d50370a111
Snigirb <145994631+Snigirb@users.noreply.github.com>
2023-10-19 15:09:07 +0600

    Method CreateStringArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 Console.WriteLine("Enter size of string array");
 int size = Convert.ToInt32(Console.ReadLine());﻿
+
+string [] CreateStringArray(int size){
+  string [] array = new string [size];
+    for (int i = 0;i<size;i++){
+        Console.Write($"Enter {i+1} element: ");
+        array[i] = new (Console.ReadLine());
+    }
+    return array;
+}
